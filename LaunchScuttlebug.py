8cae4142a9138c74a706424548971da866e73c3e
Kerry Alley <kerry_alley@yahoo.com>
2025-01-19 12:53:57 -0500

    Update LaunchScuttlebug.py
    Updated to reflect changes to BGA login procedure

--- LaunchScuttlebug.py
+++ LaunchScuttlebug.py
@@ -1,14 +1,14 @@
 #LaunchScuttlebug.py
 """
-If calling this from another script, use:
+If calling LaunchScuttlebug.py from another script, use:
 
 import LaunchScuttlebug
 user = 'your username'
 pw = 'your password' #you need to type your password here
+folder = 'folderPath' #full path of firefox webdriver
 (driver, WebDriverWait, EC, By, Keys) = LaunchScuttlebug.main(user, pw)
-
-
 """
+
 import sys, os
 import time, datetime
 from selenium import webdriver
@@ -22,44 +22,43 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
 
-
 def main(user, pw, folder):
     path = os.path.join(folder, 'geckodriver.exe')
     driver = webdriver.Firefox()
     #Set up drivers for player profiles and tables
-    bga_link = 'https://boardgamearena.com/'
+    bga_link = 'https://boardgamearena.com/account'
     
     driver.get(bga_link)
-    #print(user)
-    #print(pw)
-    
-    login_link = 'https://en.boardgamearena.com/account'
-    driver.get(login_link)
     
     wait = WebDriverWait(driver, 10)
     
-    wait.until(EC.visibility_of_element_located((By.ID, "username_input")))
-    username = driver.find_element(By.ID, "username_input")
-    username.send_keys(user)
+    # enter username
     
-    wait.until(EC.visibility_of_element_located((By.ID, "password_input")))
+    xpath_input_username = '//*[@id="account-module"]/div[3]/div[3]/div/div[2]/div/div[2]/div[1]/div/div[2]/form/div[2]/div/input'
+    wait.until(EC.visibility_of_element_located((By.XPATH, xpath_username)))
+    input_username = driver.find_element(By.XPATH, xpath_input_username)
+    input_username.send_keys(user)
     
-    password = driver.find_element(By.ID, "password_input")
-    password.send_keys(pw)
+    xpath_submit_username = '//*[@id="account-module"]/div[3]/div[3]/div/div[2]/div/div[2]/div[1]/div/div[2]/form/div[3]/div/a'
+    wait.until(EC.visibility_of_element_located((By.XPATH, xpath_submit_username)))
+    driver.find_element(By.XPATH, xpath_submit_username).click()
     
-    wait.until(EC.visibility_of_element_located((By.ID, "submit_login_button")))
-    driver.find_element(By.ID, "submit_login_button").click()
+    # enter password
+    
+    xpath_input_pw = '//*[@id="account-module"]/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div/form/div[1]/div[2]/div/input'
+    wait.until(EC.visibility_of_element_located((By.XPATH, xpath_input_pw)))
+    input_password = driver.find_element(By.XPATH, xpath_input_pw)
+    input_password.send_keys(pw)
+    
+    xpath_submit_pw = '//*[@id="account-module"]/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div/form/div[2]/div/div/a'
+    wait.until(EC.visibility_of_element_located((By.XPATH, xpath_submit_pw)))
+    driver.find_element(By.XPATH, xpath_submit_pw).click()
+    
+    # submit
+    
+    xpath_submit_login = '//*[@id="account-module"]/div[3]/div[3]/div/div[2]/div/div[2]/div[3]/div[2]/div[3]/div[3]/div/div/a'
+    wait.until(EC.visibility_of_element_located((By.XPATH, xpath_submit_login)))
+    driver.find_element(By.XPATH, xpath_submit_login).click()
+    print("login successful")
+
     return(driver, WebDriverWait, EC, By, Keys)
-    
-
-
-to_execute = """
-import LaunchScuttlebug
-user = 'your username'
-pw = 'your password' #you need to type your password here
-
-user = ''
-pw = '' #
-
-(driver, WebDriverWait, EC, By, Keys) = LaunchScuttlebug.main(user, pw)
-"""
